7735cb37d1832402d2bdf223e51068d882e8757c
Karina Camarillo <karinacamarillo4@gmail.com>
2018-10-16 14:53:05 -0700

    added comments

--- examples/checkGPIO.py
+++ examples/checkGPIO.py
@@ -2,7 +2,7 @@
 Title: Input Output
 
 Description: This program will read the inputs on the SlushEngine expansion IO. During this program you
-may apply 3.3V or ground to any of the pins to observe there change. This program can be modified to 
+may apply 3.3V or ground to any of the pins to observe their change. This program can be modified to
 include motor controls based on the inputs and outputs. 
 
 """
@@ -21,14 +21,14 @@ while(1):
 	
 	#reads pin A0 and prints the value
 	print("Pin " + label + str(i) + ": " + str(SlushEngine.getIOState(j, i)))
-	i = i + 1
+	i = i + 1   #prints values up to pin 8
 	if i == 8:
 		i = 0
 		j = j + 1
 		if j == 2:
 			j = 0
-			label = "A"
+			label = "A" #label pins associated with port A
 		elif j == 1:
-			label = "B"
+			label = "B" #label pins associated with port A
 
 	time.sleep(0.5)
