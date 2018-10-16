621ee0e4562f7aa49e4ee315a06642e8cc7fc0cb
Dylan Bienstock <dylanbien@gmail.com>
2018-10-16 11:13:53 -0700

    commented on motor.py

--- Slush/Motor.py
+++ Slush/Motor.py
@@ -7,13 +7,17 @@ from Slush.Board import *
 from Slush.Devices import L6470Registers as LReg
 import math
 
+
 class Motor(sBoard):
-    
     boardInUse = 0
-    
+
+    '''
+    takes a single parameter, motorNumber, and sets the chip select based on that input
+    input ranges from 0 to 6
+    '''
+
     def __init__(self, motorNumber):
-       
-        #setting the particular chip parameters
+
         if motorNumber == 0:
             self.chipSelect = SLX.MTR0_ChipSelect
         if motorNumber == 1:
@@ -21,30 +25,35 @@ class Motor(sBoard):
         if motorNumber == 2:
             self.chipSelect = SLX.MTR2_ChipSelect
         if motorNumber == 3:
-            self.chipSelect = SLX.MTR3_ChipSelect   
+            self.chipSelect = SLX.MTR3_ChipSelect
         if motorNumber == 4:
             self.chipSelect = SLX.MTR4_ChipSelect
         if motorNumber == 5:
             self.chipSelect = SLX.MTR5_ChipSelect
         if motorNumber == 6:
             self.chipSelect = SLX.MTR6_ChipSelect
-        #init the hardware
+        # init the hardware
         self.initPeripherals()
 
     ''' initalise the appropriate pins and buses '''
+
     def initPeripherals(self):
 
-        #check that the motors SPI is actually working
+        # check that the motors SPI is actually working
         if (self.getParam(LReg.CONFIG) == 0x2e88):
-            print ("Motor Drive Connected on GPIO " + str(self.chipSelect))
+            print("Motor Drive Connected on GPIO " + str(self.chipSelect))
             self.boardInUse = 0
         elif (self.getParam([0x1A, 16]) == 0x2c88):
-            print ("High Power Drive Connected on GPIO " + str(self.chipSelect))
+            print("High Power Drive Connected on GPIO " + str(self.chipSelect))
             self.boardInUse = 1
         else:
-            print ("communication issues; check SPI configuration and cables")
+            # prints if the SPI is not working
+            print("SPI not working; communication issues; check SPI configuration and cables")
 
-        #based on board type init driver accordingly
+        '''
+        based on board type init driver accordingly
+        gets set in the above if statements
+        '''
         if self.boardInUse == 0:
             self.setOverCurrent(2000)
             self.setMicroSteps(16)
@@ -54,23 +63,26 @@ class Motor(sBoard):
             self.setCurrent(100, 120, 140, 140)
             self.setMicroSteps(16)
 
-        #self.setParam(LReg.KVAL_RUN, 0xff)
+        # self.setParam(LReg.KVAL_RUN, 0xff)
         self.getStatus()
         self.free()
-        
+
     ''' check if the motion engine is busy '''
+
     def isBusy(self):
         status = self.getStatus()
         return (not ((status >> 1) & 0b1))
-    
+
     ''' wait for motor to finish moving *** Caution this is blocking *** '''
+
     def waitMoveFinish(self):
         status = 1
         while status:
             status = self.getStatus()
-            status = not((status >> 1) & 0b1)
+            status = not ((status >> 1) & 0b1)
 
     ''' set the microstepping level '''
+
     def setMicroSteps(self, microSteps):
         self.free()
         stepVal = 0
@@ -79,17 +91,22 @@ class Motor(sBoard):
             if microSteps == 1:
                 break
             microSteps = microSteps >> 1;
-            
+
         self.setParam(LReg.STEP_MODE, (0x00 | stepVal | LReg.SYNC_SEL_1))
 
     ''' set the threshold speed of the motor '''
+
     def setThresholdSpeed(self, thresholdSpeed):
         if thresholdSpeed == 0:
             self.setParam(LReg.FS_SPD, 0x3ff)
         else:
             self.setParam(LReg.FS_SPD, self.fsCalc(thresholdSpeed))
 
-    ''' set the current'''
+    ''' 
+    set the current
+    sets run, acc, dec, hold
+    '''
+
     def setCurrent(self, hold, run, acc, dec):
         self.setParam(LReg.KVAL_RUN, run)
         self.setParam(LReg.KVAL_ACC, acc)
@@ -124,24 +141,29 @@ class Motor(sBoard):
 
     ''' set the overcurrent threshold '''
     def setOverCurrent(self, ma_current):
-        OCValue = math.floor(ma_current/375)
+        OCValue = math.floor(ma_current / 375)
         if OCValue > 0x0f: OCValue = 0x0f
         self.setParam((LReg.OCD_TH), OCValue)
 
     ''' set the stall current level '''
     def setStallCurrent(self, ma_current):
-        STHValue = round(math.floor(ma_current/31.25))
-        if(STHValue > 0x80): STHValue = 0x80
-        if(STHValue < 0): STHValue = 9
+        STHValue = round(math.floor(ma_current / 31.25))
+        if (STHValue > 0x80): STHValue = 0x80
+        if (STHValue < 0): STHValue = 9
         self.setParam((LReg.STALL_TH), STHValue)
 
     ''' set low speed optamization '''
     def setLowSpeedOpt(self, enable):
         self.xfer(LReg.SET_PARAM | LReg.MIN_SPEED[0])
-        if enable: self.param(0x1000, 13)
-        else: self.param(0, 13)
+        if enable:
+            self.param(0x1000, 13)
+        else:
+            self.param(0, 13)
 
-    ''' start the motor spinning '''
+    ''' 
+    start the motor spinning
+    moves the motor with a direction (0/1) and a speed 
+     '''
     def run(self, dir, spd):
         speedVal = self.spdCalc(spd)
         self.xfer(LReg.RUN | dir)
@@ -171,7 +193,10 @@ class Motor(sBoard):
         self.xfer(n_stepABS >> 8)
         self.xfer(n_stepABS)
 
-    ''' move to a position with refrence to the motors current position '''
+    ''' 
+    move to a position with refrence to the motors current position 
+    '''
+
     def goTo(self, pos):
         self.xfer(LReg.GOTO)
         if pos > 0x3fffff: pos = 0x3fffff
@@ -187,7 +212,11 @@ class Motor(sBoard):
         self.xfer(pos >> 8)
         self.xfer(pos)
 
-    ''' go until switch press event occurs '''
+    ''' 
+    go until switch press event occurs 
+    takes some action that will stop when pressed
+    also takes a direction (0/1) and speed
+    '''
     def goUntilPress(self, act, dir, spd):
         self.xfer(LReg.GO_UNTIL | act | dir)
         if spd > 0x3fffff: spd = 0x3fffff
@@ -196,6 +225,7 @@ class Motor(sBoard):
         self.xfer(spd)
 
     ''' go until switch release event occurs '''
+
     def goUntilRelease(self, act, dir):
         self.xfer(LReg.RELEASE_SW | act | dir)
 
@@ -203,11 +233,17 @@ class Motor(sBoard):
     def goHome(self):
         self.xfer(LReg.GO_HOME)
 
-    ''' go to mark position '''
+    ''' 
+    go to mark position 
+    often used with setMark()
+    '''
     def goMark(self):
         self.xfer(LReg.GO_MARK)
 
-    ''' set mark point '''
+    ''' 
+    set mark point
+     often used with goMark()
+     '''
     def setMark(self, value):
 
         if value == 0: value = self.getPosition()
@@ -219,20 +255,28 @@ class Motor(sBoard):
         self.xfer(value >> 8)
         self.xfer(value)
 
-    ''' set current position to the home position '''
+    ''' 
+    set current position to the home position 
+    often used with goHome()
+    '''
     def setAsHome(self):
         self.xfer(LReg.RESET_POS)
 
     ''' reset the device to initial conditions '''
     def resetDev(self):
         self.xfer(LReg.RESET_DEVICE)
-        if self.boardInUse == 1: self.setParam([0x1A, 16], 0x3608) 
+        if self.boardInUse == 1: self.setParam([0x1A, 16], 0x3608)
 
-    ''' stop the motor using the decel '''
+    '''
+    stop the motor using the decel
+     '''
     def softStop(self):
         self.xfer(LReg.SOFT_STOP)
 
-    ''' hard stop the motor without concern for decel curve '''
+    ''' 
+    hard stop the motor
+    doesn't account for decel curve
+    '''
     def hardStop(self):
         self.xfer(LReg.HARD_STOP)
 
@@ -255,51 +299,65 @@ class Motor(sBoard):
     ''' calculates the value of the ACC register '''
     def accCalc(self, stepsPerSecPerSec):
         temp = float(stepsPerSecPerSec) * 0.137438
-        if temp > 4095.0: return 4095
-        else: return round(temp)
+        if temp > 4095.0:
+            return 4095
+        else:
+            return round(temp)
 
     ''' calculated the value of the DEC register '''
     def decCalc(self, stepsPerSecPerSec):
         temp = float(stepsPerSecPerSec) * 0.137438
-        if temp > 4095.0: return 4095
-        else: return round(temp)
+        if temp > 4095.0:
+            return 4095
+        else:
+            return round(temp)
 
     ''' calculates the max speed register '''
     def maxSpdCalc(self, stepsPerSec):
         temp = float(stepsPerSec) * 0.065536
-        if temp > 1023.0: return 1023
-        else: return round(temp)
+        if temp > 1023.0:
+            return 1023
+        else:
+            return round(temp)
 
     ''' calculates the min speed register '''
     def minSpdCalc(self, stepsPerSec):
         temp = float(stepsPerSec) * 4.1943
-        if temp > 4095.0: return 4095
-        else: return round(temp)
+        if temp > 4095.0:
+            return 4095
+        else:
+            return round(temp)
 
     ''' calculates the value of the FS speed register '''
     def fsCalc(self, stepsPerSec):
         temp = (float(stepsPerSec) * 0.065536) - 0.5
-        if temp > 1023.0: return 1023
-        else: return round(temp)
+        if temp > 1023.0:
+            return 1023
+        else:
+            return round(temp)
 
     ''' calculates the value of the INT speed register '''
     def intSpdCalc(self, stepsPerSec):
         temp = float(stepsPerSec) * 4.1943
-        if temp > 16383.0: return 16383
-        else: return round(temp)
+        if temp > 16383.0:
+            return 16383
+        else:
+            return round(temp)
 
     ''' calculate speed '''
     def spdCalc(self, stepsPerSec):
         temp = float(stepsPerSec) * 67.106
-        if temp > float(0x000fffff): return 0x000fffff
-        else: return round(temp)
+        if temp > float(0x000fffff):
+            return 0x000fffff
+        else:
+            return round(temp)
 
     ''' utility function '''
     def param(self, value, bit_len):
         ret_value = 0
 
-        byte_len = bit_len/8
-        if (bit_len%8 > 0): byte_len +=1
+        byte_len = bit_len / 8
+        if (bit_len % 8 > 0): byte_len += 1
 
         mask = 0xffffffff >> (32 - bit_len)
         if value > mask: value = mask
@@ -308,24 +366,27 @@ class Motor(sBoard):
             temp = self.xfer(value >> 16)
             ret_value |= temp << 16
         if byte_len >= 2.0:
-            temp = self.xfer(value >>8)
+            temp = self.xfer(value >> 8)
             ret_value |= temp << 8
         if byte_len >= 1.0:
             temp = self.xfer(value)
             ret_value |= temp
-       
+
         return (ret_value & mask)
 
-    ''' transfer data to the spi bus '''
+    ''' 
+    xfer = transfer
+    transfer data to the spi bus 
+    '''
     def xfer(self, data):
 
-        #mask the value to a byte format for transmision
+        # mask the value to a byte format for transmision
         data = (int(data) & 0xff)
 
-        #toggle chip select and SPI transfer
+        # toggle chip select and SPI transfer
         gpio.output(self.chipSelect, gpio.LOW)
         response = sBoard.spi.xfer2([data])
-        gpio.output(self.chipSelect, gpio.HIGH)        
+        gpio.output(self.chipSelect, gpio.HIGH)
 
         return response[0]
 
@@ -341,7 +402,7 @@ class Motor(sBoard):
 
     ''' convert twos compliment '''
     def convert(self, val):
-        if val > 0x400000/2:
+        if val > 0x400000 / 2:
             val = val - 0x400000
         return val
 
